45c52311da9e19da4059df7ca540e9e0c89e2606
rishhub <rishhub.jain@gmail.com.com>
2019-08-08 01:22:23 +0530

    winning and tie func check done - Player-tic tac toe

--- script.js
+++ script.js
@@ -29,7 +29,11 @@ startGame( );
 
 function turnClick(square){
  //console.log(square.target.id)
- turn(square.target.id, huPlayer)
+ if(typeof origBoard[square.target.id]==='number'){
+    turn(square.target.id, huPlayer)
+    if(!checkTie()) turn(bestSpot(), aiPlayer);
+ }
+
 }
 
 function turn(squareId, player){
@@ -62,7 +66,33 @@ function gameOver(gameWon){
     for(var i = 0; i<cells.length; i++){
         cells[i].removeEventListener('click', turnClick, false)
     }
+    declareWinner(gameWon.player == huPlayer ? "You Win": "YOu Loose")
 }
 
 //3. Basic AI and winner notification
-//4. Basic MinMax Logic
+function declareWinner(who){
+    document.querySelector(".endgame").style.display="block";
+    document.querySelector(".endgame .text").innerText=who;
+
+}
+function emptySquares(){
+    return origBoard.filter(s=> typeof s=='number')
+}
+
+function bestSpot(){
+    return emptySquares()[0];
+}
+
+function checkTie(){
+    if(emptySquares().length==0){
+        for(var i = 0; i<cells.length; i++){
+            cells[i].style.backgroundColor ="green";
+            cells[i].removeEventListener('click', turnClick, false);
+        }
+        declareWinner("Tie Game!");
+        return true;
+    }
+    return false;
+}
+
+//4. Algo MinMax Logic
