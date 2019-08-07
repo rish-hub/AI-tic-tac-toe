a320e0d2729070cfa3a03c3d88b289497a1b571b
rishhub <rishhub.jain@gmail.com.com>
2019-08-08 00:37:15 +0530

    Human player clicks functionality, replay func  done-tic tac toe

--- script.js
+++ script.js
@@ -1,4 +1,43 @@
 //1. Basic step
+var origBoard;
+const huPlayer = 'O';
+const aiPlayer = 'X';
+const winCombos=[
+[0,1,2],
+[3,4,5],
+[6,7,8],
+[0,3,6],
+[1,4,7],
+[2,5,8],
+[0,4,8],
+[6,4,2]
+];
+
+const cells = document.querySelectorAll('.cell');
+startGame( );
+
+ function startGame( ){
+    document.querySelector(".endgame").style.display ="none";
+    origBoard = Array.from(Array(9).keys());
+     
+     for (var i=0; i<cells.length; i++){
+        cells[i].innerText ='';
+        cells[i].style.removeProperty('background-color');
+        cells[i].addEventListener('click',turnClick,false)
+    }
+}
+
+function turnClick(square){
+ //console.log(square.target.id)
+ turn(square.target.id, huPlayer)
+}
+
+function turn(squareId, player){
+    origBoard[squareId] = player;
+    document.getElementById(squareId).innerText = player;
+}
+
+
 //2. Login For winner
 //3. Basic AI and winner notification
 //4. Basic MinMax Logic
