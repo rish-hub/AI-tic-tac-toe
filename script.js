8a833ea48bc005251461e630c6397951076c4978
rishhub <rishhub.jain@gmail.com.com>
2019-08-10 20:37:24 +0530

    wrapped up code

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ function turnClick(square){
  //console.log(square.target.id)
  if(typeof origBoard[square.target.id]==='number'){
     turn(square.target.id, huPlayer)
-    if(!checkTie()) turn(bestSpot(), aiPlayer);
+     turn(bestSpot(), aiPlayer);
  }
 
 }
@@ -41,6 +41,7 @@ function turn(squareId, player){
     document.getElementById(squareId).innerText = player;
     let gameWon = checkWin(origBoard, player)
     if(gameWon) gameOver(gameWon)
+    else checkTie()
 }
  
 //2. Logic For winner
@@ -80,7 +81,7 @@ function emptySquares(){
 }
 
 function bestSpot(){
-    return emptySquares()[0];
+    return minMax(origBoard, aiPlayer).index;
 }
 
 function checkTie(){
@@ -95,4 +96,51 @@ function checkTie(){
     return false;
 }
 
-//4. Algo MinMax Logic
+//4. Algo MinMax Logic.
+function minMax(newBoard, player){
+    var availSpots = emptySquares(newBoard);
+
+    if(checkWin(newBoard, player)){
+        return{score: -10};
+    } else if(checkWin(newBoard, aiPlayer)){
+        return{score:20};
+    } else if(availSpots.length===0){
+        return {score:0};
+    }
+    var moves= [];
+    for(var i=0; i<availSpots.length;i++){
+        var move={};
+        move.index = newBoard[availSpots[i]];
+        newBoard[availSpots[i]] = player;
+
+
+        if(player == aiPlayer){
+            var result = minMax(newBoard,huPlayer);
+            move.score =result.score;
+        } else{
+            var result = minMax(newBoard, aiPlayer);
+            move.score = result.score;
+        }
+        newBoard[availSpots[i]] = move.index;
+        moves.push(move);
+    }
+
+    var bestMove;
+    if(player == aiPlayer){
+        var bestScore =-10000;
+        for(var i = 0 ; i<moves.length; i++){
+        if(moves[i].score >bestScore){
+            bestScore = moves[i].score;
+            bestMove = i;
+        }}
+    } else {
+        var bestScore = 10000;
+        for(var i = 0 ; i<moves.length; i++){
+        if(moves[i].score <bestScore){
+            bestScore = moves[i].score;
+            bestMove = i;
+        }}
+    }
+    return moves[bestMove];
+
+}
